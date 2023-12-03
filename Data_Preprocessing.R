ef3789ce9411ce3ac4ed104cd0c3131e8c5dafe2
zack-horton <zhorton007@gmail.com>
2023-12-03 14:12:13 -0500

    updates

--- Data_Preprocessing.R
+++ Data_Preprocessing.R
@@ -4,6 +4,7 @@ library(readr)
 library(readxl)
 library(tidyverse)
 library(mltools)
+library(data.table)
 
 
 ########### Abalone ###########
@@ -12,21 +13,29 @@ abalone
 
 abalone <- abalone %>%
                 mutate(Sex = as.integer(Sex == "M"))
-abalone
+
+
+for (i in 1:ncol(abalone)){
+    abalone[is.na(abalone[,i]), i] <- mean(abalone[,i], na.rm = TRUE)
+}
+
+abalone_z <- data.frame(abalone)
+abalone_mm <- data.frame(abalone)
 
 for (k in 1:ncol(abalone)) {
     if (k != 1) {
         mu <- mean(abalone[, k])
         sigma <- sd(abalone[, k])
-        abalone[, k] <- ((abalone[, k] - mu) / sigma)
+        min <- min(abalone[, k])
+        max <- max(abalone[, k])
+        abalone_z[, k] <- ((abalone[, k] - mu) / sigma)
+        abalone_mm[, k] <- ((abalone[, k] - min) / (max - min))
     }
 }
 
-for(i in 1:ncol(abalone)){
-    abalone[is.na(abalone[,i]), i] <- mean(abalone[,i], na.rm = TRUE)
-}
+write.csv(abalone_z, "./normalized_data/abalone_z.csv", row.names = FALSE)
+write.csv(abalone_mm, "./scaled_data/abalone_mm.csv", row.names = FALSE)
 
-write.csv(abalone, "./final_data/abalone.csv", row.names = FALSE)
 ########### ###########
 
 ########### CC GEN ##############
@@ -39,15 +48,20 @@ for(i in 1:ncol(cc_gen)){
     cc_gen[is.na(cc_gen[,i]), i] <- mean(cc_gen[,i], na.rm = TRUE)
 }
 
-cc_gen
+cc_gen_z <- data.frame(cc_gen)
+cc_gen_mm <- data.frame(cc_gen)
 
 for (k in 1:ncol(cc_gen)) {
     mu <- mean(cc_gen[, k])
     sigma <- sd(cc_gen[, k])
-    cc_gen[, k] <- ((cc_gen[, k] - mu) / sigma)
+    min <- min(cc_gen[, k])
+    max <- max(cc_gen[, k])
+    cc_gen_z[, k] <- ((cc_gen[, k] - mu) / sigma)
+    cc_gen_mm[, k] <- ((cc_gen[, k] - min) / (max - min))
 }
 
-write.csv(cc_gen, "./final_data/cc_general.csv", row.names = FALSE)
+write.csv(cc_gen_z, "./normalized_data/cc_general_z.csv", row.names = FALSE)
+write.csv(cc_gen_mm, "./scaled_data/cc_general_mm.csv", row.names = FALSE)
 ########### ###########
 
 
@@ -58,15 +72,20 @@ for (i in 1:ncol(concrete)){
     concrete[is.na(concrete[,i]), i] <- mean(concrete[,i], na.rm = TRUE)
 }
 
-concrete
+concrete_z <- data.frame(concrete)
+concrete_mm <- data.frame(concrete)
 
 for (k in 1:ncol(concrete)) {
-        mu <- mean(concrete[, k])
-        sigma <- sd(concrete[, k])
-        concrete[, k] <- ((concrete[, k] - mu) / sigma)
+    mu <- mean(concrete[, k])
+    sigma <- sd(concrete[, k])
+    min <- min(concrete[, k])
+    max <- max(concrete[, k])
+    concrete_z[, k] <- ((concrete[, k] - mu) / sigma)
+    concrete_mm[, k] <- ((concrete[, k] - min) / (max - min))
 }
 
-write.csv(concrete, "./final_data/concrete_data.csv", row.names = FALSE)
+write.csv(concrete_z, "./normalized_data/concrete_data_z.csv", row.names = FALSE)
+write.csv(concrete_mm, "./scaled_data/concrete_data_mm.csv", row.names = FALSE)
 ########### ###########
 
 ########### KC Housing ###########
@@ -79,13 +98,20 @@ for (i in 1:ncol(kc_house)){
     kc_house[is.na(kc_house[,i]), i] <- mean(kc_house[,i], na.rm = TRUE)
 }
 
+kc_house_z <- data.frame(kc_house)
+kc_house_mm <- data.frame(kc_house)
+
 for (k in 1:ncol(kc_house)) {
     mu <- mean(kc_house[, k])
     sigma <- sd(kc_house[, k])
-    kc_house[, k] <- ((kc_house[, k] - mu) / sigma)
+    min <- min(kc_house[, k])
+    max <- max(kc_house[, k])
+    kc_house_z[, k] <- ((kc_house[, k] - mu) / sigma)
+    kc_house_mm[, k] <- ((kc_house[, k] - min) / (max - min))
 }
 
-write.csv(kc_house, "./final_data/kc_housing.csv", row.names = FALSE)
+write.csv(kc_house_z, "./normalized_data/kc_housing_z.csv", row.names = FALSE)
+write.csv(kc_house_mm, "./scaled_data/kc_housing_mm.csv", row.names = FALSE)
 ########### ###########
 
 
@@ -101,14 +127,21 @@ similarity <- as.data.frame(one_hot(as.data.table(similarity)))
 colnames(similarity) = gsub(",", "_", colnames(similarity))
 similarity
 
+similarity_z <- data.frame(similarity)
+similarity_mm <- data.frame(similarity)
+
 for (k in 1:ncol(similarity)) {
     if ((k %in% c(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)) == FALSE) {
     mu <- mean(similarity[, k])
     sigma <- sd(similarity[, k])
-    similarity[, k] <- ((similarity[, k] - mu) / sigma)
+    min <- min(similarity[, k])
+    max <- max(similarity[, k])
+    similarity_z[, k] <- ((similarity[, k] - mu) / sigma)
+    similarity_mm[, k] <- ((similarity[, k] - min) / (max - min))
     }
 }
 
-write.csv(similarity, "./final_data/similarity_prediction.csv", row.names = FALSE)
+write.csv(similarity_z, "./normalized_data/similarity_prediction_z.csv", row.names = FALSE)
+write.csv(similarity_mm, "./scaled_data/similarity_prediction_mm.csv", row.names = FALSE)
 
 ########### ###########
